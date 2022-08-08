150fe60fd9107c1b3db310d399a6c5b0b293a1cf
Sergio Nava <cerjionava@gmail.com>
2022-08-08 14:45:45 -0400

    added new package example

---
+++ Package_Example/request.js
@@ -0,0 +1,8 @@
+// Paquete de Node
+
+const axios = require('axios');
+
+axios.get('https://www.google.com')
+    .then((response) => console.log(response))
+    .catch((err) => console.log(err) )
+    .then(() => console.log("all done"));
